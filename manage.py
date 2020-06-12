7575e820235167a6df4db415a734413e83cd1e0d
alexinntekt <alex.inntekt@gmail.com>
2020-06-12 12:05:59 +0300

    sp

--- manage.py
+++ manage.py
@@ -5,11 +5,40 @@ filepath = "cats.mkv"
 def f1():
     media = vlc.MediaPlayer(filepath)
     media.play()
+    
+    while True:
+        pass
+
+
+def f2():
+    vlc_options = 'fullscreen'
+    vlc_instance = vlc.Instance(vlc_options)
+    # vlc.Instance.url = filepath
+    
+    player = vlc.MediaPlayer(vlc_instance)
+    player.url = filepath
+    player.play()
+
+    while True:
+        pass
+
+def f3():
+    vlc_options = "--no-audio"
+    vlc_instance = vlc.Instance(vlc_options)
+    # vlc.Instance.url = filepath
+    
+    player = vlc_instance.media_player_new() 
+    player.set_mrl(filepath)
+    player.play()
+
+    while True:
+        pass
+
 
 
 
 def run():
-    f1()
+    f3()
 
 
 run()
