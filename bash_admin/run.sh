f1e72be03741870320ea380b4f964d2e27de49ee
alexinntekt <alex.inntekt@gmail.com>
2020-06-16 15:30:27 +0300

    sp

--- bash_admin/run.sh
+++ bash_admin/run.sh
@@ -1,14 +1,15 @@
-export vlc1="398458886"
+# export vlc1="398458886"
 
 export screen_width="1920"
 export screen_height="1080"
 export filepath="cats.mkv"
 
-echo $vlc1
-
 vlc media/$filepath &
-
-sleep(2)
+export s="n"
+sleep 1s
+vlc1=$(xdotool search --onlyvisible --name vlc | sed -n 1p)
+echo $vlc1
+echo "hahahahahahah"
 
 xdotool windowsize $vlc1 100 1080
 #xdotool windowsize $vlc1 $screen_width $screen_height 
