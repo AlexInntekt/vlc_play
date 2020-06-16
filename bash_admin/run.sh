62972a1e3bdf85a7e6cf97ae80d96f9bdd2372b6
alexinntekt <alex.inntekt@gmail.com>
2020-06-16 16:36:52 +0300

    sp

--- bash_admin/run.sh
+++ bash_admin/run.sh
@@ -7,14 +7,14 @@ export filepath="cats.mkv"
 vlc media/$filepath &
 export its_pid=$!
 export s="n"
-sleep 1s
+sleep 2s
 #vlc1=$(xdotool search --onlyvisible --name vlc | sed -n 1p)
-export its_window_id=$()
-echo $vlc1
+export its_window_id=$(xdotool search --all --pid $its_pid --name vlc)
+echo $its_window_id
 echo "hahahahahahah"
 
-xdotool windowsize $vlc1 100 300
-#xdotool windowsize $vlc1 $screen_width $screen_height 
-
-xdotool windowmove $vlc1 100 50
+xdotool windowsize $its_window_id 100 300
+#xdotool windowsize $its_window_id $screen_width $screen_height 
+sleep 2s
+xdotool windowmove $its_window_id 100 50
 
