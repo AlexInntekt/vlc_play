8ccad07568e5fc2a8a34f9ec435ef940ff270aa9
alexinntekt <alex.inntekt@gmail.com>
2020-06-17 12:21:23 +0300

    sp

--- bash_admin/run.sh
+++ bash_admin/run.sh
@@ -2,19 +2,26 @@
 
 export screen_width="1920"
 export screen_height="1080"
+
+
 export filepath="cats.mkv"
 
-vlc media/$filepath &
-export its_pid=$!
-export s="n"
-sleep 2s
-#vlc1=$(xdotool search --onlyvisible --name vlc | sed -n 1p)
-export its_window_id=$(xdotool search --all --pid $its_pid --name vlc)
-echo $its_window_id
-echo "hahahahahahah"
 
-xdotool windowsize $its_window_id 300 600
-#xdotool windowsize $its_window_id $screen_width $screen_height 
-sleep 2s
-xdotool windowmove $its_window_id 700 50
+play_background()
+{
+	sleep 2s
+	vlc media/$filepath &
+	sleep 1s
+	export its_pid=$!
+	export s="n"
+	#vlc1=$(xdotool search --onlyvisible --name vlc | sed -n 1p)
+	export its_window_id=$(xdotool search --all --pid $its_pid --name vlc)
+	echo $its_window_id
 
+	xdotool windowsize $its_window_id 800 500
+	#xdotool windowsize $its_window_id $screen_width $screen_height 
+	xdotool windowmove $its_window_id 700 250
+}
+
+
+play_background 1
